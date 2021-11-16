3e3e598b4404e9fff94402423790a3d5a16b8b1a
charrr19 <93572155+charrr19@users.noreply.github.com>
2021-11-16 16:03:39 +0800

    Add files via upload

---
+++ BMI.java
@@ -0,0 +1,7 @@
+public class BMI {
+    public static void main(String[]args) {
+        int weight = 48;
+        double hight = 1.55;
+        System.out.println(weight / (hight * hight));
+    }
+}
---
+++ For_loop.java
@@ -0,0 +1,8 @@
+public class For_loop {
+    public static void main(String[] args){
+        //This is comment
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
